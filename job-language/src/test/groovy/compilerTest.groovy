cb35056a7f8944626ca7e287b8074120d85d2f18
peter <peter@europa>
2015-09-30 13:37:48 +0100

    Split out generated tests so each type of test is now in it's own Test class rather than one big one

--- job-language/src/test/groovy/compilerTest.groovy
+++ job-language/src/test/groovy/compilerTest.groovy
@@ -6,39 +6,62 @@ mkdir(destDir);
 
 generated = "@Generated(\""+new Date()+"\")";
 
-new File( "$destDir/CompilerTest.java" ).withWriter { out ->
-    out.println "package uk.trainwatch.job.lang;";
-    out.println "";
-    //out.println "import static org.junit.Assert.*;";
-    out.println "import javax.annotation.Generated;";
-    out.println "import org.junit.Test;";
-    out.println "import uk.trainwatch.job.Job;";
-    out.println "";
-    out.println "/**";
-    out.println " * Runs our test .job files through the Compiler ensuring that";
-    out.println " * the compilation actually does work.";
-    out.println " */";
-    out.println generated;
-    out.println "public class CompilerTest";
-    out.println "\textends AbstractCompilerTest";
-    out.println "{";
+def sourceDir = new File( project.basedir, "src/test/resources" );
 
-    def sourceDir = new File( project.basedir, "src/test/resources" );
-    sourceDir.eachDirRecurse {
-        it.eachFileMatch( ~/.*\.job/ ) {
-            file -> name = file.name;
-            name=name.replace(".job","");
-            out.println "";
-            out.println "\t@Test( timeout = 10000L )";
-            out.println "\tpublic void " + name + "()";
-            out.println "\t\tthrows Exception";
-            out.println "\t{";
-            out.println "\t\tJob job = compile( \"" + name + "\");";
-            out.println "\t\texecute(job);";
-            out.println "\t}";
-        }
+// Work out the test classes based on job name, eg scope1.job becomes Scope
+def tests = [] as Set;
+sourceDir.eachDirRecurse {
+    it.eachFileMatch( ~/.*\.job/ ) {
+        file -> n = file.toString();
+        if((m = n =~ /([a-zA-Z][a-zA-Z_]*?)([0-9]*?).job$/))
+            tests << m.group(1);
     }
+}
 
-    out.println "";
-    out.println "}";
+// Now create each test class
+tests.each {
+    test ->
+        className = test.replaceAll( "^([a-z])", { Object[] c->c[1].toUpperCase() } ) + "Test";
+
+        jobs = [];
+        sourceDir.eachDirRecurse {
+            it.eachFileMatch( ~/${test}.*\.job/ ) {
+                file -> jobs << file.name.replace(".job","");
+            }
+        };
+        jobs=jobs.sort();
+
+        new File( "$destDir/"+className+".java" ).withWriter { out ->
+            out.println "package uk.trainwatch.job.lang;";
+            out.println "";
+            //out.println "import static org.junit.Assert.*;";
+            out.println "import javax.annotation.Generated;";
+            out.println "import org.junit.Test;";
+            out.println "import uk.trainwatch.job.Job;";
+            out.println "";
+            out.println "/**";
+            out.println " * Runs our test .job files through the Compiler ensuring that";
+            out.println " * the compilation actually does work.";
+            out.println " */";
+            out.println generated;
+            out.println "public class "+className;
+            out.println "\textends AbstractCompilerTest";
+            out.println "{";
+
+            // Now each test case
+            jobs.each {
+                name ->
+                out.println "";
+                out.println "\t@Test( timeout = 10000L )";
+                out.println "\tpublic void " + name + "()";
+                out.println "\t\tthrows Exception";
+                out.println "\t{";
+                out.println "\t\tJob job = compile( \"" + name + "\");";
+                out.println "\t\texecute(job);";
+                out.println "\t}";
+            }
+
+            out.println "";
+            out.println "}";
+    }
 };
